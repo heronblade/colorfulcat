bc55e75e17d60a9d4eee82a8c5dae5543729f352
Joshua Byler <bylerj@gmail.com>
2013-12-07 16:14:53 -0500

    poppy

--- includes/main.js
+++ includes/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	portfolioAnchorTags();
 	// resizePort();
 
-	$(window).resize(function() { resizeHomeDiv(); checkPortfolioNav(); resizePort(); portfolioAnchorTags(); resizeAbout(); });
+	$(window).resize(function() { resizeHomeDiv(); resizePort(); portfolioAnchorTags(); resizeAbout(); });
 	// $(window).scroll(function() { checkPortfolioNav(); });
 	$(window).load(function() { resizePort(); resizeAbout(); });
 
@@ -70,12 +70,9 @@ $(document).ready(function() {
 		});
 	});
 
-
-	//when modal button is clicked, hide everything except for the description and order buttons
-
 	$('.order-btn').on('click', function() {
-		var target = $(this).data('target');
-		var origVsPrint = $(target).children('.modal-orig-print').size();
+		var target = $(this).data('target'); console.log('target ' + target);
+		var origVsPrint = $(target + ' .modal-body').children('.modal-orig-print').size(); console.log('origVsPrint ' + origVsPrint);
 
 		if (origVsPrint === 0) {
 			$('.btn-etsy').removeAttr('disabled');
@@ -85,7 +82,7 @@ $(document).ready(function() {
 		$('.modal-paypal').hide();
 	})
 
-	$('.modal-orig-print input').on('click', function() {
+	$('.modal-orig-print').on('click', function() {
 		$('.btn-etsy').removeAttr('disabled');
 		$('.btn-paypal').removeAttr('disabled');
 	});
@@ -116,11 +113,15 @@ $(document).ready(function() {
 
 	// Original vs Print Etsy
 		$('.poppy-orig').on('click', function() {
+			var paypalForm = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="business" value="DDFZEXCEVEJQG"><input type="hidden" name="lc" value="US"><input type="hidden" name="item_name" value="Poppy"><input type="hidden" name="item_number" value="pop"><input type="hidden" name="button_subtype" value="services"><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="shipping" value="5.00"><input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHosted"><div class="paypal-options"><div class="paypal-options-sizes"><input type="hidden" name="on0" value="Sizes">Sizes<select name="os0"><option value="orig">Original $175 USD</option></select></div><div class="paypal-options-paper-type"><input type="hidden" name="on1" value="Paper Type">Paper Type<select name="os1"><option value="Watercolor">Watercolor </option><option value="Photo Paper">Photo Paper </option></select></div></div><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="option_select0" value="orig"><input type="hidden" name="option_amount0" value="175"><input type="hidden" name="option_index" value="0"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>';
 			$('.poppy-etsy-btn').attr('href', 'http://www.etsy.com/listing/165057267/poppy-painting-original-watercolor?ref=shop_home_active');
+			$('.poppy-modal-paypal').html(paypalForm);
 		});
 
 		$('.poppy-print').on('click', function() {
+			var paypalForm = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="business" value="DDFZEXCEVEJQG"><input type="hidden" name="lc" value="US"><input type="hidden" name="item_name" value="Poppy"><input type="hidden" name="item_number" value="pop"><input type="hidden" name="button_subtype" value="services"><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="shipping" value="5.00"><input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHosted"><div class="paypal-options"><div class="paypal-options-sizes"><input type="hidden" name="on0" value="Sizes">Sizes<select name="os0"><option value="8x10">8x10 $25 USD</option><option value="11x14">11x14 $35 USD</option></select></div><div class="paypal-options-paper-type"><input type="hidden" name="on1" value="Paper Type">Paper Type<select name="os1"><option value="Watercolor">Watercolor </option><option value="Photo Paper">Photo Paper </option></select></div></div><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="option_select0" value="8x10"><input type="hidden" name="option_amount0" value="25"><input type="hidden" name="option_select1" value="11x14"><input type="hidden" name="option_amount1" value="35"><input type="hidden" name="option_index" value="0"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>';
 			$('.poppy-etsy-btn').attr('href', 'http://www.etsy.com/listing/164843491/flower-painting-print-from-original?ref=shop_home_active');
+			$('.poppy-modal-paypal').html(paypalForm);
 		});
 
 		$('.pumpking-patch-orig').on('click', function() {
