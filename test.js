3dae43de0e20feb7c3db3676921441304317fdb6
Joshua Byler <bylerj@gmail.com>
2013-12-19 14:02:44 -0500

    added object template
    The object template has been added, it's currently blank, and will have
    a few variables now

---
+++ test.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+
+
+	var portfolio = {
+
+		"karis": {
+			"port-popup": {
+				"href": "",
+				"title": "",
+				"src": ""
+			},
+			"caption": {
+				"h3": "",
+				"thumb-medium": "",
+				"thumb-year": "",
+				"thumb-prints": "",
+			},
+			"modal": {
+				"id": "",
+				"aria-labelledby": "",
+				"modal-title": {
+					"id": ""
+				},
+				"modal-thumb-image": {
+					"src": ""
+				},
+				"modal-order": {
+					"href": ""
+				},
+				"modal-paypal": {
+					"item_name": "",
+					"item_number": "",
+					"option_select0": "",
+					"option_amount0": "",
+					"option_select1": "",
+					"option_amount1": ""
+				},
+				"paypal-options-sizes": {
+					"value1": "",
+					"value2": ""
+				},
+				"paypal-options-paper-type": {
+					"value1": "",
+					"value2": ""
+				}
+			}
+		}
+
+	};
+
+
+	//object template
+	// var template = {
+
+	// 	"karis": {
+	// 		"port-popup": {
+	// 			"href": "",
+	// 			"title": "",
+	// 			"src": ""
+	// 		},
+	// 		"caption": {
+	// 			"h3": "",
+	// 			"thumb-medium": "",
+	// 			"thumb-year": "",
+	// 			"thumb-prints": "",
+	// 		},
+	// 		"modal": {
+	// 			"id": "",
+	// 			"aria-labelledby": "",
+	// 			"modal-title": {
+	// 				"id": ""
+	// 			},
+	// 			"modal-thumb-image": {
+	// 				"src": ""
+	// 			},
+	// 			"modal-order": {
+	// 				"href": ""
+	// 			},
+	// 			"modal-paypal": {
+	// 				"item_name": "",
+	// 				"item_number": "",
+	// 				"option_select0": "",
+	// 				"option_amount0": "",
+	// 				"option_select1": "",
+	// 				"option_amount1": ""
+	// 			},
+	// 			"paypal-options-sizes": {
+	// 				"value1": "",
+	// 				"value2": ""
+	// 			},
+	// 			"paypal-options-paper-type": {
+	// 				"value1": "",
+	// 				"value2": ""
+	// 			}
+	// 		}
+	// 	}
+		
+	// };
+
+});
+
