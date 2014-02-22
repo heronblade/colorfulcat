d075a6eb3604eae100ad738d3c1901d46d402ff8
Joshua Byler <bylerj@gmail.com>
2014-02-22 16:34:09 -0500

    couple of changes to html as well as js
    added the option to get rid of watercolor paper option if the paper is
    too big

--- includes/js/paypalform.js
+++ includes/js/paypalform.js
@@ -13,23 +13,23 @@ var paypalForm =
 		<div class="paypal-options">
 			<div class="paypal-options-sizes">
 				<input type="hidden" name="on0" value="Sizes">Sizes
-				<select name="os0">
-					<option value="5x7">5x7 $15 USD</option>
+				<select class="paypal-select" name="os0">
+					<option value="5x7">5x7 $12 USD</option>
 					<option value="8x10">8x10 $25 USD</option>
 					<option value="8.5x11">8.5x11 $28 USD</option>
-					<option value="8x12">8x12 $30 USD</option>
+					<option value="8x12">8x12 $28 USD</option>
 					<option value="9x12">9x12 $30 USD</option>
-					<option value="11x14">11x14 $40 USD</option>
-					<option value="11x15">13x19 $40 USD</option>
-					<option value="11x16">13x19 $40 USD</option>
-					<option value="13x19">13x19 $50 USD</option>
+					<option value="11x14">11x14 $35 USD</option>
+					<option value="11x15">11x15 $35 USD</option>
+					<option value="11x16">11x16 $40 USD</option>
+					<option value="13x19">13x19 $48 USD</option>
 					<option value="16x20">16x20 $60 USD</option>
-					<option value="20x30">20x30 $85 USD</option>
+					<option value="20x30">20x30 $75 USD</option>
 				</select>
 			</div>
 			<div class="paypal-options-paper-type">
 				<input type="hidden" name="on1" value="Paper Type">Paper Type
-				<select name="os1">
+				<select class="paper-select" name="os1">
 					<option value="Watercolor">Watercolor </option>
 					<option value="Photo Paper">Photo Paper </option>
 				</select>
@@ -38,23 +38,23 @@ var paypalForm =
 	</div>
 	<input type="hidden" name="currency_code" value="USD">
 	<input type="hidden" name="option_select0" value="5x7">
-	<input type="hidden" name="option_amount0" value="15">
+	<input type="hidden" name="option_amount0" value="12">
 	<input type="hidden" name="option_select1" value="8x10">
 	<input type="hidden" name="option_amount1" value="25">
 	<input type="hidden" name="option_select2" value="8.5x11">
 	<input type="hidden" name="option_amount2" value="28">
 	<input type="hidden" name="option_select3" value="8x12">
-	<input type="hidden" name="option_amount3" value="30">
+	<input type="hidden" name="option_amount3" value="28">
 	<input type="hidden" name="option_select4" value="9x12">
 	<input type="hidden" name="option_amount4" value="30">
 	<input type="hidden" name="option_select5" value="11x14">
-	<input type="hidden" name="option_amount5" value="40">
+	<input type="hidden" name="option_amount5" value="35">
 	<input type="hidden" name="option_select6" value="11x15">
-	<input type="hidden" name="option_amount6" value="40">
+	<input type="hidden" name="option_amount6" value="35">
 	<input type="hidden" name="option_select7" value="11x16">
 	<input type="hidden" name="option_amount7" value="40">
 	<input type="hidden" name="option_select8" value="13x19">
-	<input type="hidden" name="option_amount8" value="50">
+	<input type="hidden" name="option_amount8" value="48">
 	<input type="hidden" name="option_select9" value="16x20">
 	<input type="hidden" name="option_amount9" value="60">
 	<input type="hidden" name="option_select10" value="20x30">
