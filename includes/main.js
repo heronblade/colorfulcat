e2b1fd61d6177a55813f557e61d8ff963755019c
Joshua Byler <bylerj@gmail.com>
2014-03-01 13:33:37 -0500

    bag end

--- includes/main.js
+++ includes/main.js
@@ -583,6 +583,13 @@ $(document).ready(function() {
 				$('.fairytale-chapter-3-btn').show();
 			});
 
+			$('.bag-end-print').on('click', function() {
+				var paypalForm ='<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank"><input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="business" value="DDFZEXCEVEJQG"><input type="hidden" name="lc" value="US"><input type="hidden" name="item_name" value="Bag End"><input type="hidden" name="item_number" value="bag-end"><input type="hidden" name="button_subtype" value="services"><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="shipping" value="5.00"><input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHosted"><div class="paypal-options"><div class="paypal-options"><div class="paypal-options-sizes"><input type="hidden" name="on0" value="Sizes">Sizes<select class="paypal-select" name="os0"><option value="8x10">8x10 $25 USD</option><option value="9x12">9x12 $30 USD</option><option value="11x14">11x14 $35 USD</option><option value="16x20">16x20 $60 USD</option></select></div><div class="paypal-options-paper-type"><input type="hidden" name="on1" value="Paper Type">Paper Type<select class="paper-select" name="os1"><option value="Watercolor">Watercolor </option><option value="Photo Paper">Photo Paper </option></select></div></div></div><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="option_select0" value="8x10"><input type="hidden" name="option_amount0" value="25"><input type="hidden" name="option_select1" value="9x12"><input type="hidden" name="option_amount1" value="30"><input type="hidden" name="option_select2" value="11x14"><input type="hidden" name="option_amount2" value="35"><input type="hidden" name="option_select3" value="16x20"><input type="hidden" name="option_amount3" value="60"><input type="hidden" name="option_index" value="0"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" style="display:none;"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>';
+				$('.bag-end-btn').attr('href', 'https://www.etsy.com/listing/174029839/the-hobbit-painting-print-from-original?ref=related-15');
+				$('.bag-end-paypal').html(paypalForm);
+				$('.bag-end-btn').show();
+			});
+
 		//objects
 
 		//people and portraits
